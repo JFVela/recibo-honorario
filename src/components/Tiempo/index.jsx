9a9981b8f588777251988ec46a2930490967d069
JFVela <jfvela_2012@hotmail.com>
2025-01-06 23:42:39 -0500

    Corecciones

--- src/components/Tiempo/index.jsx
+++ src/components/Tiempo/index.jsx
@@ -1,30 +1,32 @@
-import * as React from 'react';
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
-import timezone from 'dayjs/plugin/timezone';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
-dayjs.tz.setDefault('America/Lima');
+dayjs.tz.setDefault("America/Lima");
 
 export default function CurrentDateTimeLima() {
-    const [currentDateTime, setCurrentDateTime] = React.useState(dayjs().tz('America/Lima'));
+  const [currentDateTime, setCurrentDateTime] = React.useState(
+    dayjs().tz("America/Lima")
+  );
 
-    React.useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentDateTime(dayjs().tz('America/Lima'));
-        }, 1000);
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDateTime(dayjs().tz("America/Lima"));
+    }, 1000);
 
-        return () => clearInterval(interval);
-    }, []);
+    return () => clearInterval(interval);
+  }, []);
 
-    return (
-        <Stack spacing={2}>
-            <Typography variant="h6">
-                {currentDateTime.format('YYYY-MM-DD hh:mm:ss A')}
-            </Typography>
-        </Stack>
-    );
+  return (
+    <Stack spacing={2}>
+      <Typography style={{ fontFamily: "Arimo" }} variant="h6">
+        <strong>Fecha: {currentDateTime.format("DD/MM HH:mm")}</strong>
+      </Typography>
+    </Stack>
+  );
 }
